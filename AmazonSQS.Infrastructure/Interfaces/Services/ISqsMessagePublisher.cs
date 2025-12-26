84f27875291e67dc3e1aa1f85334b0f44e0966ed
JGMelon22 <jgvasconcellos22@gmail.com>
2025-12-26 12:02:35 -0300

    feat: Add AmazonSQS.Infrastructure for SQS message publishing

---
+++ AmazonSQS.Infrastructure/Interfaces/Services/ISqsMessagePublisher.cs
@@ -0,0 +1,6 @@
+﻿namespace AmazonSQS.Infrastructure.Interfaces.Services;
+
+public interface ISqsMessagePublisher
+{
+    Task PublishAsync<T>(T message, string queueUrl, CancellationToken cancellationToken = default) where T : class;
+}
--- Consumer.API/Program.cs
+++ Consumer.API/Program.cs
@@ -1,4 +1,6 @@
 using Amazon.SQS;
+using AmazonSQS.Infrastructure.Configuration;
+using AmazonSQS.Infrastructure.Interfaces.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,11 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
 builder.Services.AddAWSService<IAmazonSQS>();
+
+builder.Services.Configure<SqsOptions>(
+    builder.Configuration.GetSection(SqsOptions.SqsSettings));
+
+builder.Services.AddScoped<ISqsMessagePublisher, ISqsMessagePublisher>();
 
 var app = builder.Build();
 
