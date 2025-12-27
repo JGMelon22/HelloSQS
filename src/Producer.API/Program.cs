04d7cba15b54ff6f157843d4d86a99d13194736a
JGMelon22 <jgvasconcellos22@gmail.com>
2025-12-26 22:12:51 -0300

    refactor: refactor rsolution structure

--- src/Producer.API/Program.cs
+++ src/Producer.API/Program.cs
@@ -1,6 +1,8 @@
 using Amazon.SQS;
 using AmazonSQS.Infrastructure.Configuration;
-using Consumer.Consumer.BackgroundServices;
+using AmazonSQS.Infrastructure.Interfaces.Services;
+using AmazonSQS.Infrastructure.Services;
+using Producer.API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,13 +13,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddHostedService<OrderCreatedEventConsumer>();
-
 builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
 builder.Services.AddAWSService<IAmazonSQS>();
 
 builder.Services.Configure<SqsOptions>(
     builder.Configuration.GetSection(SqsOptions.SqsSettings));
+
+builder.Services.AddScoped<ISqsMessagePublisher, SqsMessagePublisher>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
